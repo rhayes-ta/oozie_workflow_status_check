37ce6c9ecb37a68bf872685ec2b357e3030081f4
R Hayes <rhayes@tripadvisor.com>
2015-10-26 22:29:13 -0500

    Complete rewrite

--- src/check_oozie_workflows.py
+++ src/check_oozie_workflows.py
@@ -1,27 +1,5 @@
 #!/usr/bin/env python
 
-################################################
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-################################################
-
-"""
-This requires:
- python-kerberos (CentOS Base)
- python-urllib2_kerberos (EPEL)
- pytz (CentOS Base)
-root@box>yum install python-kerberos python-urllib2_kerberos pytz
-"""
 
 '''
 For info on the Oozie REST API, try 
@@ -40,214 +18,205 @@ http://prodmaster01d.hdp.tripadvisor.com:11000/oozie/v1/jobs?filter=user%3Dt4b;n
 import sys
 import urllib
 import json
-import datetime
-import urllib2
-import pytz
+import argparse
+from enum import Enum, IntEnum
+import pprint
 
 
-class OozieConnect():
-    def __init__(self, host="localhost", port="11000", security_flag="false"):
-        self.uris = {}
-        self.security_flag = security_flag
+class NagiosState(IntEnum):
+    Ok = 0
+    Warn = 1
+    Error = 2
+    Unknown = 3
+
+
+class OozieStatus(Enum):
+    FAILED = 'FAILED'
+    KILLED = 'KILLED'
+    SUSPENDED = 'SUSPENDED'
+    SUCCEEDED = 'SUCCEEDED'
+    RUNNING = 'RUNNING'
+    PREP = 'PREP'
+
+    def to_nagios(self):
+        if self is OozieStatus.FAILED:
+            return NagiosState.Error
+        elif self is OozieStatus.KILLED:
+            return NagiosState.Warn
+        elif self is OozieStatus.SUSPENDED:
+            return NagiosState.Warn
+        else:
+            return NagiosState.Ok
+
+
+def oozie_status_to_nagios(oozie_status):
+    try:
+        os = OozieStatus(oozie_status)
+        return os.to_nagios()
+    except:
+        return NagiosState.Unknown
+
+
+class Oozie():
+    def __init__(self, host='localhost', port=11000):
         self.host = host
         self.port = port
 
-#       Setter functions below here
-        self.set_uris(self.host, self.port)
-#       Get current timezone - This assumes Oozie has been setup for the system timezone
+    def _url(self):
+        return "http://%s:%d/oozie/v1/" % (self.host, self.port)
 
-    def set_uris(self, host, port, history_offset=1, history_length=100, status="SUCCEEDED"):
-        self.uris["base"] = "http://" + host + ":" + port + "/oozie"
-        self.set_job_uris(history_offset, history_length, status)
-        self.uris["status"] = self.uris["base"] + "/v1/admin/status"
-
-    def set_job_uris(self, history_offset=1, history_length=100, status="SUCCEEDED"):
-        self.uris["jobs"] = self.uris["base"] + \
-                            "/v1/jobs?jobType=wf&localtime&offset=" + str(history_offset) + \
-                            "&len=" + str(history_length) + \
-                            "&filter=status%3" + status
-
-    def set_security_flag(self, security_flag):
-        self.security_flag = security_flag
-
-    def connect(self, uri):
-        if self.security_flag == "true":
-            return self.secure_connect(self.uris[uri])
-        else:
-            return self.insecure_connect(self.uris[uri])
-
-    def secure_connect(self, uri):
-        opener = urllib2.build_opener()
-        opener.add_handler(urllib2_kerberos.HTTPKerberosAuthHandler())
-        resp = opener.open(uri)
-        a = resp.read()
-        return json.loads(a)
-
-    def insecure_connect(self, uri):
-        raw_json = urllib.urlopen(uri)
+    def _apply(self, path, params):
+        base_url = self._url()
+        url = base_url + path + '?' + urllib.urlencode(params)
+        raw_json = urllib.urlopen(url)
         return json.load(raw_json)
 
-    def test_connection(self):
-        json_object = self.connect("status")
-        if json_object[u'systemMode'] == "NORMAL":
-            return True
-        return False
+    def _checkJobtype(self,jobtype):
+        if jobtype in ('wf','coordinator','bundle'):
+            return jobtype
+        raise Exception('unknown jobtype')
 
+    def _encodeFilter(self, filter):
+        """
+        :param filter: Dict, valid keys are name, user, group, status
+        """
+        ff = [ '%s=%s' % (k,v) for k,v in filter.iteritems() ]
+        return ";".join(ff)
 
-class OozieJobs():
-    def __init__(self, oozie, time_range_minutes=60, history_length=1000):
-        self.failed_count = 0
-        self.suspended_count = 0
-        self.killed_count = 0
-        self.succeeded_count = 0
-        self.prep_count = 0
-        self.running_count = 0
-        self.workflows = []
-        self.oozie_connection = oozie
-        self.statuses = ["FAILED", "KILLED", "SUCCEEDED", "RUNNING", "SUSPENDED"]
-        self.time_range_minutes = int(time_range_minutes)
-        self.history_length = history_length
+    def jobs(self, filter={}, jobtype='wf', len=100, offset=1):
+        params = {
+            'filter':self._encodeFilter(filter),
+            'jobtype':self._checkJobtype(jobtype),
+            'len':len,
+            'offset':offset
+        }
+        return self._apply('jobs', params)
 
-    def set_time_range_minutes(self, minutes):
-        self.time_range_minutes = int(minutes)
+    def _coordinator_runs_per_day(self, timeUnit, frequency):
+        """
+        How many coordinator runs per day?  Returns float.
 
-    def get_jobs(self):
-        # iterate through the json and pull out the workflows if the time range is not met, continue getting historical
-        # jobs using 'len' until you reach the end or time_range width
-        # We'll use the job ending time as a measure
+        >>> self = Oozie()
+        >>> self._coordinator_runs_per_day(u'DAY', '1')
+        1.0
+        >>> self._coordinator_runs_per_day(u'DAY', '2')
+        0.5
+        >>> self._coordinator_runs_per_day(u'MINUTE', '360')
+        4.0
 
-        offset = 1
-        wf_ids = []
-        wf_count = 0
-        self.oozie_connection.set_job_uris(offset, self.history_length)
-        json_object = self.oozie_connection.connect("jobs")
-        for job in json_object[u'workflows']:
-            wf_count += 1
-            if self.is_within_time_range(self.time_range_minutes, job[u'createdTime']) and (job[u'id'] not in wf_ids):
+        """
 
-                wf_ids.append(job[u'id'])
+        multiplier = 1
+        if timeUnit == u'DAY':
+            multiplier = 1
+        elif timeUnit == u'MINUTE':
+            multiplier = 24*60
+        elif timeUnit == u'HOUR':
+            multiplier = 24
+        elif timeUnit == u'WEEK':
+            multiplier = 1.0/7.0
+        elif timeUnit == u'MONTH':
+            multiplier = 1.0/30.0
+        return multiplier / float(frequency)
 
-                wf_job = {
-                            "id"          : job[u'id'],
-                            "appName"     : job[u'appName'],
-                            "status"      : job[u'status'],
-                            "createdTime" : job[u'createdTime'],
-                            "startTime"   : job[u'startTime'],
-                            "endTime"     : job[u'endTime']}
+    def coordinator(self, days, filter={}):
+        fd = ExtensibleDict(filter)
+        coords = self.jobs(filter=fd.copy_with(status='RUNNING'), jobtype='coordinator')
+        cc = coords.get('coordinatorjobs')
+        for c in cc:
+            timeUnit = c.get('timeUnit')
+            frequency = c.get('frequency')
+            # determine how many workflows to fetch
+            number_to_fetch = int(days * self._coordinator_runs_per_day(timeUnit,frequency))
+            # sadly, the coordinator name (coordJobName) may not be the same as the appName
+            # in the child workflow.
+            coordPath = c.get('coordJobPath','')
+            appName = coordPath.split('/')[-1]
+            ftr = fd.copy_with(name=appName)
+            the_jobs = self.jobs(ftr, jobtype='wf',len=number_to_fetch)
+            c['workflows'] = the_jobs.get('workflows')
+            # looks like workflows are returned in reverse createdTime order, newest first. Yay.
+        return cc
 
-                self.workflows.append(wf_job)
+    # TODO Handle pagination, stepping through toplevel.offset by toplevel.len until we get to toplevel.total
 
-    def is_within_time_range(self, range_in_minutes, input_time):
-        time_now = datetime.datetime.now(pytz.timezone("GMT"))
-        wf_time = datetime.datetime.strptime(input_time, "%a, %d %b %Y %X %Z")
-        tz = pytz.timezone("GMT")
-        dt = tz.localize(datetime.datetime(wf_time.year,
-                                           wf_time.month,
-                                           wf_time.day,
-                                           wf_time.hour,
-                                           wf_time.minute,
-                                           wf_time.second))
-        range_time_minutes = datetime.timedelta(minutes=range_in_minutes)
-        if (time_now - dt) <= range_time_minutes:
-            return True
-        return False
+class ExtensibleDict(dict):
+    """
+    A dictionary which can supply an extended copy of itself, incorporating kwargs additional or replacement keys
 
-    def analyze_results(self):
-        self.suspended_count = 0
-        self.failed_count = 0
-        self.killed_count = 0
-        self.succeeded_count = 0
-        self.running_count = 0
-        self.prep_count = 0
-        # iterate through the workflows and get status
-        # For RUNNING and PREP jobs, increment their counts but don't look at their endTime since they dont have one
-        for workflow in self.workflows:
-            if workflow["status"] == "PREP":
-                self.prep_count += 1
-                continue
-            elif workflow["status"] == "RUNNING":
-                self.running_count += 1
-                continue
-            if self.is_within_time_range(self.time_range_minutes, workflow["endTime"]):
-                    if workflow["status"] == "FAILED":
-                        self.failed_count += 1
+    >>> ed = ExtensibleDict({'a':1,'b':2})
+    >>> ed == {'a':1, 'b':2}
+    True
+    >>> ed.copy_with(c=3) == {'a':1,'b':2,'c':3}
+    True
+    >>> ed == {'a': 1, 'b': 2}
+    True
+    >>> ed.copy_with(b=100) ==  {'a': 1, 'b': 100}
+    True
+    >>> ed.copy_with(c=3,delta=100,gamma='Gramma') == {'a':1,'b':2,'c':3,'delta':100,'gamma':'Gramma'}
+    True
 
-                    elif workflow["status"] == "SUSPENDED":
-                        self.suspended_count += 1
+    """
+    def __init__(self, *args, **kwargs):
+        super(ExtensibleDict, self).__init__(*args, **kwargs)
 
-                    elif workflow["status"] == "KILLED":
-                        self.killed_count += 1
-
-                    elif workflow["status"] == "SUCCEEDED":
-                        self.succeeded_count += 1
-
-        return self.failed_count + \
-               self.suspended_count + \
-               self.killed_count + \
-               self.succeeded_count + \
-               self.running_count + \
-               self.prep_count
-
-    def print_results(self):
-        total = self.failed_count +\
-                self.suspended_count +\
-                self.killed_count +\
-                self.succeeded_count +\
-                self.running_count +\
-                self.prep_count
-        # Return exit codes based on counts - order by most severe descending
-        print "Last %d minutes: FAILED: %d KILLED: %d SUSPENDED: %d SUCCEEDED: %d RUNNING: %d PREP: %d Total: %d" % (
-            self.time_range_minutes,
-            self.failed_count,
-            self.killed_count,
-            self.suspended_count,
-            self.succeeded_count,
-            self.running_count,
-            self.prep_count,
-            total)
-
-    def get_return_code(self):
-        # Generate Nagios CRITICAL
-        if self.failed_count > 0:
-            return 2
-
-        # Generate Nagios WARNING
-        elif self.killed_count > 0 or self.suspended_count > 0:
-            return 1
-
-        # Generate Nagios OK
-        else:
-            return 0
-
-    def run(self):
-        self.get_jobs()
-        self.analyze_results()
-        self.print_results()
-        return self.get_return_code()
+    def copy_with(self, **kwargs):
+        new_dict = ExtensibleDict(self)
+        new_dict.update(**kwargs)
+        return new_dict
 
 
 if __name__ == "__main__":
-    try:
-        host = sys.argv[1]
-        port = sys.argv[2]
-        kinit_truth = sys.argv[3]
-        time_range = sys.argv[4]
-        history_length = sys.argv[5]
-    except:
-        print "Arguments to check script are wrong"
-        print "Expecting [1] host [2] port [3] kerberos ruth (true|false) [4] range in minutes [5] number of jobs "
-        sys.exit(0)
 
-    if kinit_truth:
-        import urllib2_kerberos
-    
-    oozie_connection = OozieConnect(host, port, kinit_truth)
-    # if oozie_connection.test_connection():
-    #     print "Good Connection"
-    jobs = OozieJobs(oozie_connection, time_range, history_length)
+    parser = argparse.ArgumentParser(description='Get status of Oozie jobs')
+    parser.add_argument('host')
+    parser.add_argument('-p','--port', type=int, default=11000)
+    parser.add_argument('-l','--limit', type=int, default=100, help='Limit on number of coordinators to check')
+    parser.add_argument('-u', '--user', help='User name to filter')
+    parser.add_argument('-n', '--name', help='Name to filter')
+    parser.add_argument('-d', '--days', type=int, help="how many days back to examine workflow history", default=3)
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                      help='increase output verbosity (use up to 3 times)')
+
+    args = parser.parse_args()
+
     try:
-        rc = jobs.run()
-    except IOError:
-        print "Couldn't connect to Oozie on %s:%s" % (host, port)
-        sys.exit(1)
-    sys.exit(rc)
+        oozie = Oozie(args.host)
+        filter={}
+        if args.user:
+            filter['user'] = args.user
+        if args.name:
+            filter['name'] = args.name
+        cc = oozie.coordinator(args.days, filter)
+
+        exitCode = NagiosState.Ok.value
+
+        for c in cc:
+            coordJobName = c.get('coordJobName')
+            coordStatus = c.get('status')
+
+            workflows = c.get('workflows', [])
+            for wf in workflows:
+                wfStatus = wf.get('status')
+                wfId = wf.get('id')
+                wfStartTime = wf.get('startTime')
+                nag = oozie_status_to_nagios(wfStatus)
+
+                if args.verbose > 2:
+                    print wf
+                elif args.verbose == 2:
+                    print coordJobName, coordStatus, wfStatus, wfId, '"%s"' % wfStartTime
+                exitCode = max(exitCode,nag)
+
+            if args.verbose == 1:
+                if workflows:
+                    print coordJobName, len(workflows), (max( [ oozie_status_to_nagios(wf.get('status')) for wf in workflows] )).name
+                else:
+                    print coordJobName, len(workflows), NagiosState.Unknown.name
+
+        sys.exit(exitCode)
+
+    except Exception as problem:
+        print problem
+        sys.exit(NagiosState.Unknown)
+
